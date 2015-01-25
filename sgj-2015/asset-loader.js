9e1cf77c690d7cc0d60d7ea50fc3bab67fa6ebbd
Dean Panayotov <deanpanayotov@gmail.com>
2015-01-25 14:23:48 +0200

    Added players and povement.

--- sgj-2015/asset-loader.js
+++ sgj-2015/asset-loader.js
@@ -2,4 +2,16 @@
  * Created by Dean Panayotov Local on 25.1.2015 г..
  */
 var a_i_background = new Image();
-a_i_background.src = 'assets/bg_canvas.jpg';
+a_i_background.src = 'assets/canvas/bg.png';
+var a_i_highlights = new Image();
+a_i_highlights.src = 'assets/canvas/highlights.png';
+var a_i_house_cut = new Image();
+a_i_house_cut.src = 'assets/canvas/house_cut.png';
+
+var a_i_avatars = [new Image(), new Image(), new Image(), new Image()];
+a_i_avatars[0].src = 'assets/canvas/players/1.png';
+a_i_avatars[1].src = 'assets/canvas/players/2.png';
+a_i_avatars[2].src = 'assets/canvas/players/3.png';
+a_i_avatars[3].src = 'assets/canvas/players/4.png';
+
+
--- sgj-2015/sgj-2015.js
+++ sgj-2015/sgj-2015.js
@@ -2,6 +2,14 @@ var canvas = document.getElementById("canvas");
 canvas.width = WIDTH;
 canvas.height = HEIGHT;
 
+var numPlayers = 4;
+
+
+var players = [];
+for(var i=0; i<numPlayers; i++){
+    players[i] = new Player(PL_X_POS[0][i],FLOOR_HEIGHT[0],a_i_avatars[i], PL_OFFSETS[i][0], PL_OFFSETS[i][1]);
+}
+
 //canvas.width = wrapper.clientWidth;
 //canvas.height = wrapper.clientHeight;
 
@@ -35,17 +43,25 @@ var step = function() {
 };
 
 var update = function() {
-
+    for(var i = 0; i<numPlayers; i++){
+        players[i].update();
+    }
 };
 
 var render = function() {
     renderBackground();
+    for(var i = 0; i<numPlayers; i++){
+        players[i].render(ctx);
+    }
 };
 
 var renderBackground = function(){
     ctx.drawImage(a_i_background,0,0,a_i_background.width,a_i_background.height);
+    ctx.drawImage(a_i_highlights,0,0,a_i_highlights.width,a_i_highlights.height);
+    ctx.drawImage(a_i_house_cut,0,0,a_i_house_cut.width,a_i_house_cut.height);
 }
 
 var restart = function(){
 
 }
+
