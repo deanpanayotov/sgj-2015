d977f92f2dd074507e730da06d768ffdf4ece662
Dean Panayotov <deanpanayotov@gmail.com>
2015-01-25 03:45:36 +0200

    Popup added.

---
+++ sgj-2015/onclicks.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Dean Panayotov Local on 25.1.2015 г..
+ */
+var displayBox = function (){
+    document.getElementById('light').style.display='block';
+    document.getElementById('fade').style.display='block'
+    console.log("bbb");
+};
+
+var hideBox = function(){
+    document.getElementById('light').style.display='none';
+    document.getElementById('fade').style.display='none';
+    console.log("aaa");
+};
+
