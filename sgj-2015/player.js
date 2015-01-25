e44b730647f82dd93be47ac536f5adb79691800a
Dean Panayotov <deanpanayotov@gmail.com>
2015-01-25 17:49:29 +0200

    Fixed merge issue.

--- sgj-2015/player.js
+++ sgj-2015/player.js
@@ -5,14 +5,14 @@
 var id_counter;
 var playerColors = [ CL_RED, CL_BLUE, CL_GREEN, CL_YELLOW];
 
-var Player = function(x, y, avatar, offx, offy){
+var Player = function(x, y, avatar, offx, offy) {
 
     this.MAXYVAR = 30;
     this.SPEED = 2;
 
     this.id = id_counter++;
-    this.name = "Player #"+this.id;
-    this.color = playerColors[this.id%4];
+    this.name = "Player #" + this.id;
+    this.color = playerColors[this.id % 4];
     this.yVar = 0;
     this.x = x;
     this.y = y;
@@ -23,56 +23,57 @@ var Player = function(x, y, avatar, offx, offy){
     this.newy = y;
     this.yDir = this.SPEED;
     this.moving = false;
-    this.setNewPos = function(nx, ny){
+    this.setNewPos = function (nx, ny) {
         this.newx = nx;
         this.newy = ny;
         this.moving = true;
     }
 
-    this.move = function(){
+    this.move = function () {
         this.movingCheck();
-        if(this.moving){
-            if(this.x < this.newx){
+        if (this.moving) {
+            if (this.x < this.newx) {
                 this.x += this.SPEED;
-            }else if(this.x > this.newx){
+            } else if (this.x > this.newx) {
                 this.x -= this.SPEED;
             }
-            if(this.y < this.newy){
+            if (this.y < this.newy) {
                 this.y += this.SPEED;
-            }else if(this.y > this.newy){
+            } else if (this.y > this.newy) {
                 this.y -= this.SPEED;
             }
         }
     }
 
-    this.movingCheck = function() {
-        if(Math.abs(this.x-this.newx) <= this.SPEED+1){
+    this.movingCheck = function () {
+        if (Math.abs(this.x - this.newx) <= this.SPEED + 1) {
             this.x = this.newx;
         }
-        if(Math.abs(this.y-this.newy) <= this.SPEED+1){
+        if (Math.abs(this.y - this.newy) <= this.SPEED + 1) {
             this.y = this.newy;
         }
-        if(this.x == this.newx && this.y == this.newy){
+        if (this.x == this.newx && this.y == this.newy) {
             this.moving = false;
         }
     }
 
-    this.update = function() {
-        if(this.moving){
+    this.update = function () {
+        if (this.moving) {
             this.updateYVar();
             this.move();
         }
-        if(this.yVar<0){
+        if (this.yVar < 0) {
             this.updateYVar();
         }
     }
-    this.updateYVar = function(){
+    this.updateYVar = function () {
         this.yVar -= this.yDir;
-        if(this.yVar <= -30 || this.yVar >= 0){
+        if (this.yVar <= -30 || this.yVar >= 0) {
             this.yDir *= -1;
         }
     }
 
-    this.render = function(ctx){
-        ctx.drawImage(this.avatar,this.x+this.offx,this.y+this.yVar-this.offy,this.avatar.width,this.avatar.height);
+    this.render = function (ctx) {
+        ctx.drawImage(this.avatar, this.x + this.offx, this.y + this.yVar - this.offy, this.avatar.width, this.avatar.height);
     }
+}
